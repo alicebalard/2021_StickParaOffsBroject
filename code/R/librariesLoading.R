947b873f04c222dc47ce897c235c1564fe60b5de
Alice Balard <alice.cam.balard@gmail.com>
2022-04-05 17:23:57 +0100

    rerun DMS/DMR with brother pairs as covariate

--- code/R/librariesLoading.R
+++ code/R/librariesLoading.R
@@ -1,41 +1,61 @@
 ## load all libraries needed for the project
 
-library(cowplot)
-library(dendextend) # The package dendextend contains many functions for changing the appearance of a dendrogram and for comparing dendrograms.
-library(devtools)
-library(dplyr)
-library(emmeans) ## for post-hoc Tukey tests
-library(genomation) ## for annotation
-# BiocManager::install("GenomicFeatures")
-library(GenomicFeatures) ## for annotation
-library(ggeffects) # to plot random effects predictions
-library(ggplot2)
-library(ggpubr) ## to merge ggplot2 plots
-theme_set(theme_pubr())
-library(goeveg) # find the best number of dimensions for NMDS
-library(ggsignif) ## for significance bars on ggplot
-library(grid)
-library(gridExtra)
-library(lme4) ## for mixed models
-library(lmtest) # for lrtests
-library(magrittr)      # provides the %>% operator
-library(methylKit)
-library(nlme) ## for mixed models
-# install_github("pmartinezarbizu/pairwiseAdonis/pairwiseAdonis")
+list.of.packages <- c(
+    "cowplot",
+    "dendextend", # The package dendextend contains many functions for changing the appearance of a dendrogram and for comparing dendrograms.
+    "devtools",
+    "dplyr",
+    "emmeans", ## for post-hoc Tukey tests
+    "genomation", ## for annotation
+    "ggeffects", # to plot random effects predictions
+    "ggplot2",
+    "ggpubr", ## to merge ggplot2 plots
+    "goeveg", # find the best number of dimensions for NMDS
+    "ggsignif", ## for significance bars on ggplot
+    "grid",
+    "gridExtra",
+    "lme4", ## for mixed models
+    "lmtest", # for lrtests
+    "magrittr",      # provides the %>% operator
+    "methylKit",
+    "nlme", ## for mixed models
+    "plyr", # for join (keep row order",
+    "png",
+    "qualpalr",# extra palettes
+    "RColorBrewer", # for colors in Venn diagrams
+    "reshape2",
+    "tidyverse",  # tidyverse will pull in ggplot2, readr, other useful libraries
+    "vegan", ## for Adonis
+    "VennDiagram")
+
+ipak <- function(pkg){
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  if (length(new.pkg))
+    install.packages(new.pkg, dependencies = TRUE)
+   sapply(pkg, require, character.only = TRUE)
+}
+ipak(list.of.packages)
+
+## github packages manual check
+if (!"pairwiseAdonis" %in% installed.packages()[, "Package"]) {
+    devtools::install_github("pmartinezarbizu/pairwiseAdonis/pairwiseAdonis")
+}
 library(pairwiseAdonis)
-library(plyr) # for join (keep row order)
-library(png)
-library(qualpalr)# extra palettes
-library(RColorBrewer) # for colors in Venn diagrams
-library(reshape2)
-library(tidyverse)     # tidyverse will pull in ggplot2, readr, other useful libraries
-library(vegan) ## for Adonis
-library(VennDiagram)
-#BiocManager::install("WGCNA")
-library(WGCNA)
+
+## Biocmanager packages 
+list.bioc <- c("WGCNA",
+               "GenomicFeatures") ## for annotation
+ipak2 <- function(pkg){
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  if (length(new.pkg))
+    BiocManager::install(new.pkg)
+   sapply(pkg, require, character.only = TRUE)
+}
+ipak2(list.bioc)
 
 ## offspring colors for all kind of plots
 colOffs <- c("#ffe67f", "#ff6300","#a8caff","#a800d4")
 
+theme_set(theme_pubr())
 
 
