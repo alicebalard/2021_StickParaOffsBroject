2f901189a6839ddd15dccdce6b583ac615e4af38
Alice Balard <alice.cam.balard@gmail.com>
2022-04-27 14:19:40 +0100

    add extra DMS/DMR for control-control and inf-inf

--- code/R/R04.1_getDifferentialMethylation.R
+++ code/R/R04.1_getDifferentialMethylation.R
@@ -21,8 +21,12 @@ source("R02.2_loadMethyldata.R")
 
 ### 3 comparisons to do:
 ## PARENTS trt-ctrl
-## G2-control G1 trt-ctrl
-## G2-infected G1 trt-ctrl ## HYP: this will be different
+## G1control G2 trt-ctrl
+## G1infected G2 trt-ctrl ## HYP: this will be different
+
+## Extra 2 for paternal effect:
+## G1control-G2control vs G1infected-G2control
+## G1control-G2infected vs G1infected-G2infected
 
 ################################################################
 ## Correspondance trtG1G2 and numerical values used by MethylKit
@@ -42,68 +46,94 @@ source("R02.2_loadMethyldata.R")
 
 ## Summarize methylation information over tiling windows with a sliding window size of 100 bases and step size of 100 bases
 
-#############
-## G1
-## CpG covered in half trt group, parental DMS:
-nrow(uniteCov6_G1_woSexAndUnknowChrOVERLAP) # methylBase object with 1001880 rows
-tiles_G1_half = tileMethylCounts(uniteCov6_G1_woSexAndUnknowChrOVERLAP, win.size=100,step.size=100,cov.bases = 10)
-nrow(tiles_G1_half) # methylBase object with 20348 rows
-
-## CpG covered in all fish, parental DMS:
-uniteCovALL_G1_woSexAndUnknowChr <- reorganize(methylObj = uniteCovALL_woSexAndUnknowChr,
-                   treatment = fullMetadata$trtG1G2_NUM[
-                                                     fullMetadata$trtG1G2_NUM %in% c(1,4)],
-                   sample.ids = fullMetadata$ID[
-                                                 fullMetadata$trtG1G2_NUM %in% c(1,4)])
-nrow(uniteCovALL_G1_woSexAndUnknowChr) # methylBase object with 55530 rows (CpG covered)
-tiles_G1_ALL = tileMethylCounts(uniteCovALL_G1_woSexAndUnknowChr,win.size=100,step.size=100,cov.bases = 10)
-nrow(tiles_G1_ALL) # methylBase object with 413 rows
+################
+##### G1
+##### CpG covered in half trt group, parental DMS:
+###nrow(uniteCov6_G1_woSexAndUnknowChrOVERLAP) # methylBase object with 1001880 rows
+###tiles_G1_half = tileMethylCounts(uniteCov6_G1_woSexAndUnknowChrOVERLAP, win.size=100,step.size=100,cov.bases = 10)
+###nrow(tiles_G1_half) # methylBase object with 20348 rows
+###
+##### CpG covered in all fish, parental DMS:
+###uniteCovALL_G1_woSexAndUnknowChr <- reorganize(methylObj = uniteCovALL_woSexAndUnknowChr,
+###                   treatment = fullMetadata$trtG1G2_NUM[
+###                                                     fullMetadata$trtG1G2_NUM %in% c(1,4)],
+###                   sample.ids = fullMetadata$ID[
+###                                                 fullMetadata$trtG1G2_NUM %in% c(1,4)])
+###nrow(uniteCovALL_G1_woSexAndUnknowChr) # methylBase object with 55530 rows (CpG covered)
+###tiles_G1_ALL = tileMethylCounts(uniteCovALL_G1_woSexAndUnknowChr,win.size=100,step.size=100,cov.bases = 10)
+###nrow(tiles_G1_ALL) # methylBase object with 413 rows
+###
+#################
+##### G2 from control fathers
+##### CpG covered in half trt group, G2 from G1 control DMS:
+###uniteCov14_G2_woSexAndUnknowChr_G1CONTROL <- reorganize(methylObj = uniteCov14_G2_woSexAndUnknowChrOVERLAP,
+###                                                        treatment = fullMetadata_OFFS$trtG1G2_NUM[
+###                                                          fullMetadata_OFFS$trtG1G2_NUM %in% c(5,6)],
+###                                                        sample.ids = fullMetadata_OFFS$ID[
+###                                                          fullMetadata_OFFS$trtG1G2_NUM %in% c(5,6)])
+###nrow(uniteCov14_G2_woSexAndUnknowChr_G1CONTROL) # methylBase object with 1001880 rows
+###tiles_G2_G1CONTROL_half = tileMethylCounts(uniteCov14_G2_woSexAndUnknowChr_G1CONTROL,win.size=100,step.size=100,cov.bases = 10)
+###nrow(tiles_G2_G1CONTROL_half) # methylBase object with 20348 rows
+###
+##### CpG covered in all fish, G2 from G1 control DMS:
+###uniteCovALL_G2_woSexAndUnknowChr_G1CONTROL <- reorganize(methylObj = uniteCovALL_woSexAndUnknowChr,
+###                   treatment = fullMetadata$trtG1G2_NUM[
+###                                                     fullMetadata$trtG1G2_NUM %in% c(5,6)],
+###                   sample.ids = fullMetadata$ID[
+###                                                 fullMetadata$trtG1G2_NUM %in% c(5,6)])
+###nrow(uniteCovALL_G2_woSexAndUnknowChr_G1CONTROL) # methylBase object with 55530 rows (CpG covered)
+###
+###tiles_G2_G1CONTROL_ALL = tileMethylCounts(uniteCovALL_G2_woSexAndUnknowChr_G1CONTROL,win.size=100,step.size=100,cov.bases = 10)
+###nrow(tiles_G2_G1CONTROL_ALL) # methylBase object with 413 rows
+###
+#################
+##### G2 from infected fathers
+##### CpG covered in half trt group, G2 from G1 infected DMS:
+###uniteCov14_G2_woSexAndUnknowChr_G1INFECTED <- reorganize(methylObj = uniteCov14_G2_woSexAndUnknowChrOVERLAP,
+###                                                        treatment = fullMetadata_OFFS$trtG1G2_NUM[
+###                                                          fullMetadata_OFFS$trtG1G2_NUM %in% c(2,3)],
+###                                                        sample.ids = fullMetadata_OFFS$ID[
+###                                                          fullMetadata_OFFS$trtG1G2_NUM %in% c(2,3)])
+###nrow(uniteCov14_G2_woSexAndUnknowChr_G1INFECTED) # methylBase object with 1001880 rows
+###
+###tiles_G2_G1INFECTED_half = tileMethylCounts(uniteCov14_G2_woSexAndUnknowChr_G1INFECTED,win.size=100,step.size=100,cov.bases = 10)
+###nrow(tiles_G2_G1INFECTED_half) # methylBase object with 20348 rows
+###
+##### CpG covered in all fish, G2 from G1 infected DMS:
+###uniteCovALL_G2_woSexAndUnknowChr_G1INFECTED <- reorganize(methylObj = uniteCovALL_woSexAndUnknowChr,
+###                   treatment = fullMetadata$trtG1G2_NUM[
+###                                                     fullMetadata$trtG1G2_NUM %in% c(2,3)],
+###                   sample.ids = fullMetadata$ID[
+###                                                 fullMetadata$trtG1G2_NUM %in% c(2,3)])
+###nrow(uniteCovALL_G2_woSexAndUnknowChr_G1INFECTED) # methylBase object with 55530 rows (CpG covered)
+###tiles_G2_G1INFECTED_ALL = tileMethylCounts(uniteCovALL_G2_woSexAndUnknowChr_G1INFECTED,win.size=100,step.size=100,cov.bases = 10)
+###nrow(tiles_G2_G1INFECTED_ALL) # methylBase object with 413 rows
+###
 
 ##############
-## G2 from control fathers
-## CpG covered in half trt group, G2 from G1 control DMS:
-uniteCov14_G2_woSexAndUnknowChr_G1CONTROL <- reorganize(methylObj = uniteCov14_G2_woSexAndUnknowChrOVERLAP,
+## G2 control from both control & infected fathers
+## CpG covered in half trt group
+uniteCov14_G1bothTrt_G2control_woSexAndUnknowChr <- reorganize(methylObj = uniteCov14_G2_woSexAndUnknowChrOVERLAP,
                                                         treatment = fullMetadata_OFFS$trtG1G2_NUM[
-                                                          fullMetadata_OFFS$trtG1G2_NUM %in% c(5,6)],
+                                                          fullMetadata_OFFS$trtG1G2_NUM %in% c(2,5)],
                                                         sample.ids = fullMetadata_OFFS$ID[
-                                                          fullMetadata_OFFS$trtG1G2_NUM %in% c(5,6)])
-nrow(uniteCov14_G2_woSexAndUnknowChr_G1CONTROL) # methylBase object with 1001880 rows
-tiles_G2_G1CONTROL_half = tileMethylCounts(uniteCov14_G2_woSexAndUnknowChr_G1CONTROL,win.size=100,step.size=100,cov.bases = 10)
-nrow(tiles_G2_G1CONTROL_half) # methylBase object with 20348 rows
+                                                          fullMetadata_OFFS$trtG1G2_NUM %in% c(2,5)])
+nrow(uniteCov14_G1bothTrt_G2control_woSexAndUnknowChr) # methylBase object with 1001880 rows
 
-## CpG covered in all fish, G2 from G1 control DMS:
-uniteCovALL_G2_woSexAndUnknowChr_G1CONTROL <- reorganize(methylObj = uniteCovALL_woSexAndUnknowChr,
-                   treatment = fullMetadata$trtG1G2_NUM[
-                                                     fullMetadata$trtG1G2_NUM %in% c(5,6)],
-                   sample.ids = fullMetadata$ID[
-                                                 fullMetadata$trtG1G2_NUM %in% c(5,6)])
-nrow(uniteCovALL_G2_woSexAndUnknowChr_G1CONTROL) # methylBase object with 55530 rows (CpG covered)
+tiles_G1bothTrt_G2control_half = tileMethylCounts(uniteCov14_G1bothTrt_G2control_woSexAndUnknowChr,win.size=100,step.size=100,cov.bases = 10)
+nrow(tiles_G1bothTrt_G2control_half) # methylBase object with 20348 rows
 
-tiles_G2_G1CONTROL_ALL = tileMethylCounts(uniteCovALL_G2_woSexAndUnknowChr_G1CONTROL,win.size=100,step.size=100,cov.bases = 10)
-nrow(tiles_G2_G1CONTROL_ALL) # methylBase object with 413 rows
-
-##############
-## G2 from infected fathers
-## CpG covered in half trt group, G2 from G1 infected DMS:
-uniteCov14_G2_woSexAndUnknowChr_G1INFECTED <- reorganize(methylObj = uniteCov14_G2_woSexAndUnknowChrOVERLAP,
+## G2 infected from both control & infected fathers
+## CpG covered in half trt group
+uniteCov14_G1bothTrt_G2infected_woSexAndUnknowChr <- reorganize(methylObj = uniteCov14_G2_woSexAndUnknowChrOVERLAP,
                                                         treatment = fullMetadata_OFFS$trtG1G2_NUM[
-                                                          fullMetadata_OFFS$trtG1G2_NUM %in% c(2,3)],
+                                                          fullMetadata_OFFS$trtG1G2_NUM %in% c(3,6)],
                                                         sample.ids = fullMetadata_OFFS$ID[
-                                                          fullMetadata_OFFS$trtG1G2_NUM %in% c(2,3)])
-nrow(uniteCov14_G2_woSexAndUnknowChr_G1INFECTED) # methylBase object with 1001880 rows
+                                                          fullMetadata_OFFS$trtG1G2_NUM %in% c(3,6)])
+nrow(uniteCov14_G1bothTrt_G2infected_woSexAndUnknowChr) # methylBase object with 1001880 rows
 
-tiles_G2_G1INFECTED_half = tileMethylCounts(uniteCov14_G2_woSexAndUnknowChr_G1INFECTED,win.size=100,step.size=100,cov.bases = 10)
-nrow(tiles_G2_G1INFECTED_half) # methylBase object with 20348 rows
-
-## CpG covered in all fish, G2 from G1 infected DMS:
-uniteCovALL_G2_woSexAndUnknowChr_G1INFECTED <- reorganize(methylObj = uniteCovALL_woSexAndUnknowChr,
-                   treatment = fullMetadata$trtG1G2_NUM[
-                                                     fullMetadata$trtG1G2_NUM %in% c(2,3)],
-                   sample.ids = fullMetadata$ID[
-                                                 fullMetadata$trtG1G2_NUM %in% c(2,3)])
-nrow(uniteCovALL_G2_woSexAndUnknowChr_G1INFECTED) # methylBase object with 55530 rows (CpG covered)
-tiles_G2_G1INFECTED_ALL = tileMethylCounts(uniteCovALL_G2_woSexAndUnknowChr_G1INFECTED,win.size=100,step.size=100,cov.bases = 10)
-nrow(tiles_G2_G1INFECTED_ALL) # methylBase object with 413 rows
+tiles_G1bothTrt_G2infected_half = tileMethylCounts(uniteCov14_G1bothTrt_G2infected_woSexAndUnknowChr,win.size=100,step.size=100,cov.bases = 10)
+nrow(tiles_G1bothTrt_G2infected_half) # methylBase object with 20348 rows
 
 ##############################################################################
 ## Calculate DMS/DMR accounting for covariates: brotherPairID and sex (new 04/04/22!)
@@ -120,62 +150,83 @@ getDiffMeth <- function(myuniteCov, myMetadata){
   return(myDMS_15pc)
 }
 
-## Comparison 1: BASELINE -> Parents (control vs infected) 
-## CpG covered in HALF fish per group
-DMS15pc_G1_half <- getDiffMeth(uniteCov6_G1_woSexAndUnknowChrOVERLAP, fullMetadata_PAR)
-saveRDS(DMS15pc_G1_half, file = "../../data/DiffMeth/DMS15pc_G1_half.RDS")
+###### Comparison 1: BASELINE -> Parents (control vs infected) 
+###### CpG covered in HALF fish per group
+####DMS15pc_G1_half <- getDiffMeth(uniteCov6_G1_woSexAndUnknowChrOVERLAP, fullMetadata_PAR)
+####saveRDS(DMS15pc_G1_half, file = "../../data/DiffMeth/DMS15pc_G1_half.RDS")
+####
+####DMR15pc_G1_half <- getDiffMeth(tiles_G1_half, fullMetadata_PAR)
+####saveRDS(DMR15pc_G1_half, file = "../../data/DiffMeth/DMR15pc_G1_half.RDS")
+####
+###### CpG covered in ALL fish
+####DMS15pc_G1_ALL <- getDiffMeth(uniteCovALL_G1_woSexAndUnknowChr, fullMetadata_PAR)
+####saveRDS(DMS15pc_G1_ALL, file = "../../data/DiffMeth/DMS15pc_G1_ALL.RDS")
+####
+####DMR15pc_G1_ALL <- getDiffMeth(tiles_G1_ALL, fullMetadata_PAR)
+####DMR15pc_G1_ALL # EMPTY
+####
+###### Comparison 2: Offspring
+###### 2.1. Should be like baseline -> G2 from G1 control (control vs infected)
+####
+###### CpG covered in HALF fish per group
+####DMS15pc_G2_controlG1_half <- getDiffMeth(myuniteCov = uniteCov14_G2_woSexAndUnknowChr_G1CONTROL,
+####                                     myMetadata = fullMetadata_OFFS[fullMetadata_OFFS$trtG1G2_NUM %in% c(5,6),])
+####saveRDS(DMS15pc_G2_controlG1_half, file = "../../data/DiffMeth/DMS15pc_G2_controlG1_half.RDS")
+####
+####DMR15pc_G2_controlG1_half <- getDiffMeth(tiles_G2_G1CONTROL_half, fullMetadata_OFFS[fullMetadata_OFFS$trtG1G2_NUM %in% c(5,6),])
+####saveRDS(DMR15pc_G2_controlG1_half, file = "../../data/DiffMeth/DMR15pc_G2_controlG1_half.RDS")
+####
+###### CpG covered in ALL fish
+####DMS15pc_G2_controlG1_ALL <- getDiffMeth(myuniteCov = uniteCovALL_G2_woSexAndUnknowChr_G1CONTROL,
+####                                        myMetadata = fullMetadata_OFFS[fullMetadata_OFFS$trtG1G2_NUM %in% c(5,6),])
+####
+####DMS15pc_G2_controlG1_ALL
+####saveRDS(DMS15pc_G2_controlG1_ALL, file = "../../data/DiffMeth/DMS15pc_G2_controlG1_ALL.RDS")
+####
+####DMR15pc_G2_controlG1_ALL <- getDiffMeth(tiles_G2_G1CONTROL_ALL, fullMetadata_OFFS[fullMetadata_OFFS$trtG1G2_NUM %in% c(5,6),])
+####saveRDS(DMR15pc_G2_controlG1_ALL, file = "../../data/DiffMeth/DMR15pc_G2_controlG1_ALL.RDS")
+####
+###### 2.2. Should be DIFFERENT if there is a paternal effect -> G2 from G1 infected (control vs infected) 
+####
+###### CpG covered in HALF fish per group
+####DMS15pc_G2_infectedG1_half <- getDiffMeth(myuniteCov = uniteCov14_G2_woSexAndUnknowChr_G1INFECTED, 
+####                                      myMetadata = fullMetadata_OFFS[fullMetadata_OFFS$trtG1G2_NUM %in% c(2,3),])
+####  saveRDS(DMS15pc_G2_infectedG1_half, file = "../../data/DiffMeth/DMS15pc_G2_infectedG1_half.RDS")
+####
+####DMR15pc_G2_infectedG1_half <- getDiffMeth(tiles_G2_G1INFECTED_half, fullMetadata_OFFS[fullMetadata_OFFS$trtG1G2_NUM %in% c(2,3),])
+####saveRDS(DMR15pc_G2_infectedG1_half, file = "../../data/DiffMeth/DMR15pc_G2_infectedG1_half.RDS")
+####
+###### CpG covered in ALL fish
+####DMS15pc_G2_infectedG1_ALL <- getDiffMeth(myuniteCov = uniteCovALL_G2_woSexAndUnknowChr_G1INFECTED,
+####                                        myMetadata = fullMetadata_OFFS[fullMetadata_OFFS$trtG1G2_NUM %in% c(2,3),])
+####saveRDS(DMS15pc_G2_infectedG1_ALL, file = "../../data/DiffMeth/DMS15pc_G2_infectedG1_ALL.RDS")
+####
+####DMR15pc_G2_controlG1_ALL <- getDiffMeth(tiles_G2_G1INFECTED_ALL, fullMetadata_OFFS[fullMetadata_OFFS$trtG1G2_NUM %in% c(2,3),])
+####saveRDS(DMR15pc_G2_controlG1_ALL, file = "../../data/DiffMeth/DMR15pc_G2_infectedG1_ALL.RDS")
+####
 
-DMR15pc_G1_half <- getDiffMeth(tiles_G1_half, fullMetadata_PAR)
-saveRDS(DMR15pc_G1_half, file = "../../data/DiffMeth/DMR15pc_G1_half.RDS")
+### 3.1. Should be DIFFERENT if there is a paternal effect -> G2 control from both G1; CpG covered in HALF fish per group
+## DMS:
+DMS15pc_G1_controlG2_half <- getDiffMeth(myuniteCov = uniteCov14_G1bothTrt_G2control_woSexAndUnknowChr,
+                                      myMetadata = fullMetadata_OFFS[fullMetadata_OFFS$trtG1G2_NUM %in% c(2,5),])
+saveRDS(DMS15pc_G1_controlG2_half, file = "../../data/DiffMeth/DMS15pc_G1_controlG2_half.RDS")
 
-## CpG covered in ALL fish
-DMS15pc_G1_ALL <- getDiffMeth(uniteCovALL_G1_woSexAndUnknowChr, fullMetadata_PAR)
-saveRDS(DMS15pc_G1_ALL, file = "../../data/DiffMeth/DMS15pc_G1_ALL.RDS")
+## DMR:
+DMR15pc_G1_controlG2_half <- getDiffMeth(tiles_G1bothTrt_G2control_half, fullMetadata_OFFS[fullMetadata_OFFS$trtG1G2_NUM %in% c(2,5),])
+saveRDS(DMR15pc_G1_controlG2_half, file = "../../data/DiffMeth/DMR15pc_G1_controlG2_half.RDS")
 
-DMR15pc_G1_ALL <- getDiffMeth(tiles_G1_ALL, fullMetadata_PAR)
-DMR15pc_G1_ALL # EMPTY
+### 3.2. Should be DIFFERENT if there is a paternal effect -> G2 infected from both G1; CpG covered in HALF fish per group
+##DMS:
+DMS15pc_G1_infectedG2_half <- getDiffMeth(myuniteCov = uniteCov14_G1bothTrt_G2infected_woSexAndUnknowChr,
+                                      myMetadata = fullMetadata_OFFS[fullMetadata_OFFS$trtG1G2_NUM %in% c(3,6),])
+saveRDS(DMS15pc_G1_infectedG2_half, file = "../../data/DiffMeth/DMS15pc_G1_infectedG2_half.RDS")
 
-## Comparison 2: Offspring
-## 2.1. Should be like baseline -> G2 from G1 control (control vs infected)
-
-## CpG covered in HALF fish per group
-DMS15pc_G2_controlG1_half <- getDiffMeth(myuniteCov = uniteCov14_G2_woSexAndUnknowChr_G1CONTROL,
-                                     myMetadata = fullMetadata_OFFS[fullMetadata_OFFS$trtG1G2_NUM %in% c(5,6),])
-saveRDS(DMS15pc_G2_controlG1_half, file = "../../data/DiffMeth/DMS15pc_G2_controlG1_half.RDS")
-
-DMR15pc_G2_controlG1_half <- getDiffMeth(tiles_G2_G1CONTROL_half, fullMetadata_OFFS[fullMetadata_OFFS$trtG1G2_NUM %in% c(5,6),])
-saveRDS(DMR15pc_G2_controlG1_half, file = "../../data/DiffMeth/DMR15pc_G2_controlG1_half.RDS")
-
-## CpG covered in ALL fish
-DMS15pc_G2_controlG1_ALL <- getDiffMeth(myuniteCov = uniteCovALL_G2_woSexAndUnknowChr_G1CONTROL,
-                                        myMetadata = fullMetadata_OFFS[fullMetadata_OFFS$trtG1G2_NUM %in% c(5,6),])
-
-DMS15pc_G2_controlG1_ALL
-saveRDS(DMS15pc_G2_controlG1_ALL, file = "../../data/DiffMeth/DMS15pc_G2_controlG1_ALL.RDS")
-
-DMR15pc_G2_controlG1_ALL <- getDiffMeth(tiles_G2_G1CONTROL_ALL, fullMetadata_OFFS[fullMetadata_OFFS$trtG1G2_NUM %in% c(5,6),])
-saveRDS(DMR15pc_G2_controlG1_ALL, file = "../../data/DiffMeth/DMR15pc_G2_controlG1_ALL.RDS")
-
-## 2.2. Should be DIFFERENT if there is a paternal effect -> G2 from G1 infected (control vs infected) 
-
-## CpG covered in HALF fish per group
-DMS15pc_G2_infectedG1_half <- getDiffMeth(myuniteCov = uniteCov14_G2_woSexAndUnknowChr_G1INFECTED, 
-                                      myMetadata = fullMetadata_OFFS[fullMetadata_OFFS$trtG1G2_NUM %in% c(2,3),])
-  saveRDS(DMS15pc_G2_infectedG1_half, file = "../../data/DiffMeth/DMS15pc_G2_infectedG1_half.RDS")
-
-DMR15pc_G2_infectedG1_half <- getDiffMeth(tiles_G2_G1INFECTED_half, fullMetadata_OFFS[fullMetadata_OFFS$trtG1G2_NUM %in% c(2,3),])
-saveRDS(DMR15pc_G2_infectedG1_half, file = "../../data/DiffMeth/DMR15pc_G2_infectedG1_half.RDS")
-
-## CpG covered in ALL fish
-DMS15pc_G2_infectedG1_ALL <- getDiffMeth(myuniteCov = uniteCovALL_G2_woSexAndUnknowChr_G1INFECTED,
-                                        myMetadata = fullMetadata_OFFS[fullMetadata_OFFS$trtG1G2_NUM %in% c(2,3),])
-saveRDS(DMS15pc_G2_infectedG1_ALL, file = "../../data/DiffMeth/DMS15pc_G2_infectedG1_ALL.RDS")
-
-DMR15pc_G2_controlG1_ALL <- getDiffMeth(tiles_G2_G1INFECTED_ALL, fullMetadata_OFFS[fullMetadata_OFFS$trtG1G2_NUM %in% c(2,3),])
-saveRDS(DMR15pc_G2_controlG1_ALL, file = "../../data/DiffMeth/DMR15pc_G2_infectedG1_ALL.RDS")
+## DMR:
+DMR15pc_G1_infectedG2_half <- getDiffMeth(tiles_G1bothTrt_G2infected_half, fullMetadata_OFFS[fullMetadata_OFFS$trtG1G2_NUM %in% c(3,6),])
+saveRDS(DMR15pc_G1_infectedG2_half, file = "../../data/DiffMeth/DMR15pc_G1_infectedG2_half.RDS")
 
 ## stop here:
-stop("We stop here for now") # to run getDiffMeth on Apocrita cause it's LONG
+## stop("We stop here for now") # to run getDiffMeth on Apocrita cause it's LONG
 
 ## To do on bash: rename with DATES 
 ## for f in DMS15pc*; do mv "$f" "$(echo "$f" | sed s/.RDS/_25feb22.RDS/)"; done
--- code/S04_GynoGen_functionalAnnotation.sh
+++ code/S04_GynoGen_functionalAnnotation.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Functional annotation of gynogen genome
 #$ -pe smp 1
-#$ -l h_vmem=5G
-#$ -l h_rt=1:0:0
+#$ -l h_vmem=10G
+#$ -l h_rt=240:0:0
 #$ -o /data/SBCS-EizaguirreLab/Alice/StickParaBroOff/GIT_StickParaOffsBroject/code/run_gynofuncal.stdout
 #$ -e /data/SBCS-EizaguirreLab/Alice/StickParaBroOff/GIT_StickParaOffsBroject/code/run_gynofuncal.stderr
 #$ -V
@@ -24,16 +24,20 @@ cd $DIR
 ## Extract proteins from new genome with AGAT. 
 
 ## the GFF output from Maker2 needed to be filtered first. Parsing the gff output from maker2 was extremely slow (took >53 hours), but then it took only 3 minutes on a filtered gff. I needed to remove unnecessary gff entries that maker2 had included (match, expressed_sequence_match, protein_match, contig) (Source: https://www.biostars.org/p/9477640/):
-grep -Pv "\tmatch_part\t" $GFF1 | grep -Pv "\tprotein_match\t" | grep -Pv "\texpressed_sequence_match\t" | grep -Pv "\tmatch\t" | grep -Pv "\tcontig\t" > $GFF1.streamlined_for_AGAT.gff
+
+# grep -Pv "\tmatch_part\t" $GFF1 | grep -Pv "\tprotein_match\t" | grep -Pv "\texpressed_sequence_match\t" | grep -Pv "\tmatch\t" | grep -Pv "\tcontig\t" > $GFF1.streamlined_for_AGAT.gff
 
 ### To extract and translate the coding regions:
-agat_sp_extract_sequences.pl -g $GFF1.streamlined_for_AGAT.gff -f $GYNOGENOME1 -p -o $DIR/Gynogen_pchrom_assembly_all_PROTEINS.faa
+# agat_sp_extract_sequences.pl -g $GFF1.streamlined_for_AGAT.gff -f $GYNOGENOME1 -p -o $DIR/Gynogen_pchrom_assembly_all_PROTEINS.faa
 
 ## Add functional annotations and meta-data
 # Run blastp command:
 
-makeblastdb -in $UNIPROTDB -dbtype prot
-blastp -query $DIR/Gynogen_pchrom_assembly_all_PROTEINS.faa -db $UNIPROTDB -num_threads 8 -evalue 1e-6 -max_hsps 1 -max_target_seqs 1 -outfmt 6 -out $DIR/output_onlyuniprot.blastp
+# makeblastdb -in $UNIPROTDB -dbtype prot
+# blastp -query $DIR/Gynogen_pchrom_assembly_all_PROTEINS.faa -db $UNIPROTDB -num_threads 8 -evalue 1e-6 -max_hsps 1 -max_target_seqs 1 -outfmt 6 -out $DIR/output_onlyuniprot.blastp
 
 ## Integrate functional annotations into structural annotations. 
-maker_functional_gff $UNIPROTDB $DIR/output_onlyuniprot.blastp $GFF1 > $DIR/Gy_allnoM_rd3.maker_apocrita.noseq_corrected_CURATED.gff
+# maker_functional_gff $UNIPROTDB $DIR/output_onlyuniprot.blastp $GFF1 > $DIR/Gy_allnoM_rd3.maker_apocrita.noseq_corrected_CURATED.gff
+
+## Convert gff to bed12 for further analyses
+agat_convert_sp_gff2bed.pl -gff $DIR/Gy_allnoM_rd3.maker_apocrita.noseq_corrected_CURATED.gff -o $DIR/Gy_allnoM_rd3.maker_apocrita.noseq_corrected_CURATED.bed12
