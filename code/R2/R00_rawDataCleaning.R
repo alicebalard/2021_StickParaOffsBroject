4c4e2265f2b955e123d580685ffd097f7e099b5d
Alice <alice.cam.balard@gmail.com>
2022-12-21 17:37:02 +0000

    clean script by script

---
+++ code/R2/R00_rawDataCleaning.R
@@ -0,0 +1,95 @@
+## Cleaning raw data, associate a father to each offspring clutch
+## 1st of April 2022 (updated Dec22)
+source("librariesLoading.R")
+## load custom functions
+source("customRfunctions.R")
+
+## Load data
+dataG1 <- read.csv("../../data/raw data Joshka Kostas/data-G1-Kostas_JK.csv")
+dataAll <- read.csv("../../data/raw data Joshka Kostas/Kostas_G2_info.csv")
+key <- read.csv("../../data/raw data Joshka Kostas/key_matchV48_clutchID_G2_Alice.csv")
+
+## NOTES:
+# Family in the G2 file is the family for Generat P (parent) and the father’s family for Generat O (offspring). 
+# The family for Generat O is actually clutch_ID in data-G1-Kostas. The link between the two is "key"
+
+as.character(dataG1$Idplate) %in% dataAll$matchV48
+as.character(dataG1$Idplate) 
+dataAll$matchV48
+
+## NAs in dataG1 are in fact 0
+dataG1$number.of.worms[is.na(dataG1$number.of.worms)] <- 0
+
+## Merge parental data and check that there was no mistakes on crucial variables
+cleanDF <- merge(data.frame(matchV48 = dataG1$Idplate,
+                            FamilyOfFather = dataG1$FamilyOfFather,
+                            clutch.ID = dataG1$clutch.ID,
+                            HK.W = dataG1$HK.W/10,
+                            No.Worms = dataG1$number.of.worms,
+                            Wnettofin = dataG1$W.net,
+                            Slfin = dataG1$SL),
+                 dataAll, 
+                 all = TRUE)
+
+## Add offspring clutch ID matching with matchV48
+cleanDF <- merge(data.frame(matchV48 = as.character(key$matchV48),
+                            clutch.ID.G2 = key$clutch.ID),
+                 cleanDF,
+                 all=TRUE)
+
+cleanDF$clutch.ID <- gsub("NA", "", paste0(cleanDF$clutch.ID, cleanDF$clutch.ID.G2))
+
+## Rm clutchIDG2
+cleanDF <- cleanDF[!names(cleanDF) %in% "clutch.ID.G2"]
+
+## Add father ID
+cleanDF <- merge(na.omit(data.frame(FamilyOfFather = cleanDF$FamilyOfFather,
+                                    clutch.ID = cleanDF$clutch.ID,
+                                    fatherID = cleanDF$ID)),
+                 cleanDF,
+                 all = TRUE)
+
+## Remove fathers that were not sequenced
+cleanDF <- cleanDF[!is.na(cleanDF$Generat),]
+
+## Split offsping and fathers to add fatherID and FamilyOfFather to offspring
+cleanDFG1 <- cleanDF[cleanDF$Generat %in% "P",]
+cleanDFG2 <- cleanDF[cleanDF$Generat %in% "O",]
+
+cleanDFG2 <- merge(data.frame(FamilyOfFather = cleanDFG1$FamilyOfFather,
+                              clutch.ID = cleanDFG1$clutch.ID,
+                              fatherID = cleanDFG1$fatherID),
+                   cleanDFG2, by=c("clutch.ID"))
+
+# rm redundant empty columns
+cleanDFG2 <- cleanDFG2[!names(cleanDFG2) %in% c("FamilyOfFather.y", "fatherID.y")]
+names(cleanDFG2)[names(cleanDFG2) %in% c("FamilyOfFather.x", "fatherID.x")] <- c("FamilyOfFather", "fatherID")
+
+## Bind G1 and G2
+cleanDF <- rbind(cleanDFG1, cleanDFG2)
+
+## rm fatherID from fathers to avoid confusion
+cleanDF$fatherID[cleanDF$Generat %in% "P"] <- NA
+
+## Export cleaned dataset
+write.csv(cleanDF, "../../data/cleanedRawData144fishG1G2.csv", quote = F, row.names = F)
+
+## For Alice's study: we need only G2 fish and their matching G1
+table(cleanDF$clutch.ID, cleanDF$Generat)
+# some fathers don't have G2: discard
+
+table(cleanDF$clutch.ID, cleanDF$Generat) %>% unclass() %>% data.frame() %>% 
+  dplyr::filter(O > 0) %>% row.names() -> clutch.ID2keep
+
+# 132 fish
+clean132 <- cleanDF[cleanDF$clutch.ID %in% clutch.ID2keep,] 
+
+table(clean132$Generat) 
+# 116 offspring, 16 parents
+
+## Export cleaned dataset for reduced study (only G1 which have offspring)
+write.csv(cleanDF[cleanDF$clutch.ID %in% clutch.ID2keep,], 
+          "../../data/cleanedRawData132fishG1G2.csv", quote = F, row.names = F)
+
+## clean workspace
+rm(clutch.ID2keep, dataAll, dataG1, cleanDF, cleanDFG1, cleanDFG2, clean132, key)
---
+++ code/R2/R01_calculateSequencingStats.R
@@ -0,0 +1,135 @@
+## Alice Balard
+## November 2021 (updated Dec22)
+
+# Each script sources the previous script of the pipeline if needed
+source("R00_rawDataCleaning.R")
+
+#########################
+## Data loading & prep ##
+#########################
+# Raw data including Kostas previous results to compare and have the data
+rawData <- read.csv("../../data/cleanedRawData132fishG1G2.csv")
+rawData$SampleID <- rawData$ID
+
+## Raw reads quality check
+file <- read.csv("../../data/multiqc_general_stats_rawReads.txt") 
+file$SampleID = gsub("_L00.*","", gsub(".*-L1_","", file$Sample.Name))
+names(file) <- c("Sample.Name", "percent_duplicates_rawReads", "percent_GC_rawReads", "M.Seqs_rawReads", "SampleID")
+
+## Trimmed reads quality check
+fileT <- read.table("../../data/multiqc_general_stats_trimmedReadsCutadapt.txt", header = T) 
+names(fileT) <- gsub("FastQC_mqc.generalstats.fastqc.", "", names(fileT))
+fileT$SampleID = gsub("_L00.*","", gsub(".*-L1_","", fileT$Sample))
+names(fileT) <- c("Sample", "percent_duplicates_trimmedReads", "percent_gc_trimmedReads", "avg_sequence_length_trimmedReads", "percent_fails_trimmedReads", "total_sequences_trimmedReads", "SampleID")   
+
+## Mapping efficiency after Bismark
+mappBismarck <- read.table(file = "../../data/Report_mapping_efficiency_Bismark.txt")
+mappDatBIS <- data.frame(SampleID = gsub("_L00*.","", gsub(".*-L1_","", gsub("_R1_001_trimmed_cutadapt_bismark_bt2_SE_report.txt", "", mappBismarck$V1))),
+                         MappingEfficiency = as.numeric(gsub("%", "", mappBismarck$V4)))
+
+## Mapping efficiency after BSBolt
+mappBSBolt <- read.table(file = "../../data/Report_mapping_efficiency_BSBolt.txt")
+mappDatBSB <- data.frame(SampleID = gsub("_L00*.","", gsub(".*-L1_","", gsub("_R1_001_trimmed_cutadapt.fastq.gz", "", mappBSBolt$V1))),
+                         MappingEfficiency = as.numeric(mappBSBolt$V3))
+
+## Nbr of methylated sites:
+methylBSdf <- read.delim("../../data/BSBolt_Methcall_Methstats_nbrMethylatedCpGperSample.txt", header = FALSE)
+names(methylBSdf) <- c("Sample_Name", "NbrMethylatedCpG")
+methylBSdf$SampleID <- gsub("_L00*.","", gsub(".*-L1_","", gsub("_R1_001", "", methylBSdf$Sample_Name)))
+
+## Merge metadata:
+fullMetadata <- merge(merge(merge(merge(methylBSdf, mappDatBSB), file), fileT), rawData)
+fullMetadata <- fullMetadata[!names(fullMetadata) %in% c("Sample.Name", "Sample")]
+names(fullMetadata)[names(fullMetadata) %in% "MappingEfficiency"] <- "MappingEfficiency%BSBoldvsGynogen"
+
+# Mapping stats:
+## Number of raw reads:
+mean(fullMetadata$M.Seqs_rawReads) # 11.02M reads
+qnorm(0.975)*sd(fullMetadata$M.Seqs_rawReads)/sqrt(nrow(fullMetadata)) # +/-0.38
+
+## Mapping efficiency with BSBolt:
+mean(fullMetadata$MappingEfficiency) # 85.6
+qnorm(0.975)*sd(fullMetadata$MappingEfficiency)/sqrt(nrow(fullMetadata)) # +/- 0.51
+
+## After exploration of raw data (multiQC files), we decide to remove 4 samples
+# with bad quality and/or less than 6M reads after trimming ("S12", "S22", "S110", "S118", "S142")
+fullMetadata <- fullMetadata[!fullMetadata$ID %in% c("S12", "S22", "S110", "S118", "S142"),]
+
+sum(table(fullMetadata$Generat))
+table(fullMetadata$Generat)
+# N=127: 111 offspring + 16 parents
+
+## Export summary table:
+write.csv(fullMetadata, "../../data/fullMetadata127_Alice.csv", row.names=FALSE, quote=FALSE)
+
+#######################################
+## Comparison between the 2 aligners ##
+#######################################
+rerun = FALSE
+if (rerun == TRUE){
+  
+  # Plot mapping efficiency by reads number:
+  hist(mappDatBIS$MappingEfficiency, breaks = 100)
+  hist(mappDatBSB$MappingEfficiency, breaks = 100)
+  
+  # Mean and 95% confidence interval: 71.8 +/-0.59 for Bismark
+  mean(mappDatBIS$MappingEfficiency)
+  qnorm(0.975)*sd(mappDatBIS$MappingEfficiency)/sqrt(nrow(mappDatBIS))
+  
+  ## 85.4 +/- 0.51 for BSBolt
+  mean(mappDatBSB$MappingEfficiency)
+  qnorm(0.975)*sd(mappDatBSB$MappingEfficiency)/sqrt(nrow(mappDatBSB))
+  
+  ## plot:
+  AllDFBIS <- merge(file, mappDatBIS)
+  AllDFBSB <- merge(file, mappDatBSB)
+  
+  AllDFBIS$MappingEfficiencyBIS <- AllDFBIS$MappingEfficiency
+  AllDFBSB$MappingEfficiencyBSB <- AllDFBSB$MappingEfficiency
+  AllDFBSB <- AllDFBSB[,-6]
+  AllDFBIS <- AllDFBIS[,-6]
+  
+  AllDF <- merge(AllDFBIS, AllDFBSB)
+  
+  plotComparisonAligners <- ggplot(AllDF, aes(x=MappingEfficiencyBIS, y=MappingEfficiencyBSB, label = SampleID))+
+    geom_point() +
+    geom_label_repel() +
+    theme_bw()+
+    theme(legend.position = "none") +
+    geom_abline(slope=1, intercept=0, col = "red") +
+    coord_cartesian(xlim = c(48,90), ylim = c(48,90)) +
+    xlab("Mapping efficiency Bismark (% aligned reads)")+
+    ylab("Mapping efficiency BSBolt (% aligned reads)")
+  pdf("../../data/fig/plot_comparison_aligners.pdf")
+  plotComparisonAligners
+  dev.off()
+  
+  png("../../data/fig/plot_comparison_aligners.png")
+  plotComparisonAligners
+  dev.off()
+  
+  ggplot(AllDFBSB, aes(x=M.Seqs_rawReads, y=MappingEfficiencyBSB, label = SampleID))+
+    geom_point() +
+    geom_label_repel() +
+    theme_bw()+
+    theme(legend.position = "none")
+  
+  # Not good sample: S12 (baaad fastqc too)
+  AllDFBSB[AllDFBSB$SampleName %in% "S12",] # 68.96% mappability
+  
+  AllDFBSB$X..Dups <- as.numeric(gsub("%", "", AllDFBSB$X..Dups))
+  
+  ggplot(AllDFBSB, aes(x=X..Dups, y=MappingEfficiencyBSB, label = SampleID))+
+    geom_point() +
+    geom_label_repel() +
+    theme_bw()+
+    theme(legend.position = "none")
+  
+  # S12 is a NE-control offspring, to be removed for bad quality
+  # the 2 other samples with rather low read counts have an ok quality,
+  # and belong to 2 different groups (E_control, NE_exposed) so it should
+  # not bias the further steps too much
+}
+
+## clean workspace
+rm(file, fileT, mappDatBIS, mappBismarck, mappBSBolt, mappDatBSB, methylBSdf, rawData)
