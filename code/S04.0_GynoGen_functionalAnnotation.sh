c1a3f61f6d63f3e014d63fab2f78d07ee86724f5
Alice Balard <alice.cam.balard@gmail.com>
2024-09-13 13:44:26 +0100

    improve annot

--- code/S04.0_GynoGen_functionalAnnotation.sh
+++ code/S04.0_GynoGen_functionalAnnotation.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 # Functional annotation of gynogen genome
 #$ -pe smp 1
-#$ -l h_vmem=3G
-#$ -l h_rt=240:0:0
-#$ -o /data/SBCS-EizaguirreLab/Alice/StickParaBroOff/StickParaOffsBroject/code/logs/S04_run_gynofuncal.stdout
-#$ -e /data/SBCS-EizaguirreLab/Alice/StickParaBroOff/StickParaOffsBroject/code/logs/S04_run_gynofuncal.stderr
+#$ -l h_vmem=10G
+#$ -l h_rt=24:0:0
+#$ -o /data/SBCS-EizaguirreLab/Alice/StickParaBroOff/StickParaOffsBroject/code/logs/S04_run_gynofuncal_2024.stdout
+#$ -e /data/SBCS-EizaguirreLab/Alice/StickParaBroOff/StickParaOffsBroject/code/logs/S04_run_gynofuncal_2024.stderr
 #$ -V
-
-## with 10 nodes and 10G for interproscan run one day
 
 ## Source: http://weatherby.genetics.utah.edu/MAKER/wiki/index.php/MAKER_Tutorial_for_WGS_Assembly_and_Annotation_Winter_School_2018#Example_MAKER_Annotation_Project
 module load blast+
@@ -30,10 +28,12 @@ cd $DIR
 
 # grep -Pv "\tmatch_part\t" $GFF1 | grep -Pv "\tprotein_match\t" | grep -Pv "\texpressed_sequence_match\t" | grep -Pv "\tmatch\t" | grep -Pv "\tcontig\t" > $GFF1.streamlined_for_AGAT.gff
 
-NEWGFF=$DIR/Gy_allnoM_rd3.maker_apocrita.noseq_corrected.gff.streamlined_for_AGAT.gff
+GFF2=$GFF1.streamlined_for_AGAT.gff
 
 ### To extract and translate the coding regions:
 # agat_sp_extract_sequences.pl -g $GFF1.streamlined_for_AGAT.gff -f $GYNOGENOME1 -p -o $DIR/Gynogen_pchrom_assembly_all_PROTEINS.faa
+
+CODINGFAA=$DIR/Gynogen_pchrom_assembly_all_PROTEINS.faa
 
 ## Add functional annotations and meta-data
 ### Perform external functional analysis. 
@@ -44,65 +44,59 @@ NEWGFF=$DIR/Gy_allnoM_rd3.maker_apocrita.noseq_corrected.gff.streamlined_for_AGA
 # makeblastdb -in $UNIPROTDB -dbtype prot
 # blastp -query $DIR/Gynogen_pchrom_assembly_all_PROTEINS.faa -db $UNIPROTDB -num_threads 8 -evalue 1e-6 -max_hsps 1 -max_target_seqs 1 -outfmt 6 -out $DIR/Gynogen_pchrom_assembly_all_PROTEINS.faa.blast.out.blastp
 
-BLASTPout=$DIR/Gynogen_pchrom_assembly_all_PROTEINS.faa.blast.out.blastp
-
 # STEP 2: Run interproscan (improvement Sept 2024 from Charley)
 ## Install: https://interproscan-docs.readthedocs.io/en/latest/UserDocs.html
 
-## Run for a day or 2, separate script
+## with 10 nodes and 10G for interproscan run one day
 ## qsub /data/SBCS-EizaguirreLab/Alice/StickParaBroOff/StickParaOffsBroject/code/S04.1_interproscan.sh
  
-## Integrate functional annotations into structural annotations.
+###############################################################
+## Integrate functional annotations into structural annotations
 # INTERPROout=$DIR/Gynogen_pchrom_assembly_all_PROTEINS_noast.faa.interproscan.out.tsv
 
-INTERPROout=$DIR/Gynogen_pchrom_assembly_all_PROTEINS_noast.faa.interproscan.out.tsv
+## Input: gff to be modified, coding fasta, blasp output, interproscan tsv output
+## Output: a functionally annotated gff3 and bed12
 
+FASTA=Gynogen_pchrom_assembly_all_PROTEINS_noast.faa ## noast=asterisks removed
+##FASTA=testInput.100.faa
 
-### Rename gene models and other data.
+GFF3=$GFF2
+## GFF3=testInput.100.gff3
 
-maker_map_ids --prefix GMOD_ --justify 8 S_pombe_chrIII.all.gff > map
-map_gff_ids map S_pombe_chrIII.all.gff
-map_fasta_ids map S_pombe_chrIII.all.maker.proteins.fasta
-map_fasta_ids map S_pombe_chrIII.all.maker.transcripts.fasta
-map_data_ids map S_pombe_chrIII.all.maker.proteins.fasta.blastp
-map_data_ids map S_pombe_chrIII.all.maker.proteins.fasta.iprscan
+BLASTPout=$DIR/Gynogen_pchrom_assembly_all_PROTEINS.faa.blast.out.blastp
+## BLASTPout=testInput.100.faa.blast.out.blastp
 
-Integrate functional annotations into structural annotations.
+IPRout=$DIR/Gynogen_pchrom_assembly_all_PROTEINS_noast.faa.interproscan.out.tsv ## noast: we removed asterisk for interproscan
+##IPRout=testInput.100.faa.interproscan.out.tsv
 
-ipr_update_gff S_pombe_chrIII.all.gff S_pombe_chrIII.all.maker.proteins.fasta.iprscan > S_pombe_chrIII.all.2.gff
-iprscan2gff3 S_pombe_chrIII.all.maker.proteins.fasta.iprscan S_pombe_chrIII.all.gff >> S_pombe_chrIII.all.2.gff 
-mv S_pombe_chrIII.all.2.gff S_pombe_chrIII.all.gff
+## Integrate functional annotations into structural annotations.
 
-maker_functional_gff Swissprot_no_S_pombe.fasta S_pombe_chrIII.all.maker.proteins.fasta.blastp S_pombe_chrIII.all.gff > S_pombe_chrIII.all.2.gff
-maker_functional_fasta Swissprot_no_S_pombe.fasta S_pombe_chrIII.all.maker.proteins.fasta.blastp S_pombe_chrIII.all.maker.proteins.fasta > S_pombe_chrIII.all.maker.proteins.2.fasta
-maker_functional_fasta Swissprot_no_S_pombe.fasta S_pombe_chrIII.all.maker.proteins.fasta.blastp S_pombe_chrIII.all.maker.transcripts.fasta > S_pombe_chrIII.all.maker.proteins.2.fasta
-mv S_pombe_chrIII.all.2.gff S_pombe_chrIII.all.gff
-mv S_pombe_chrIII.all.maker.proteins.2.fasta S_pombe_chrIII.all.maker.proteins.fasta
-mv S_pombe_chrIII.all.maker.proteins.fasta S_pombe_chrIII.all.maker.proteins.2.fasta
-
-
-########## Previous
+## 1. add iprscan
 
 ##  ipr_update_gff - Takes InterproScan (iptrscan) output and maps domain IDs and GO terms to the Dbxref and Ontology_term attributes in the GFF3 file.
-
-#### TBC here
-
-ipr_update_gff $NEWGFF $IPRout > $DIR/temp.gff
+ipr_update_gff $GFF3 $IPRout > $GFF3.funtemp.gff3
 
 ## iprscan2gff3 - Takes InterproScan (iprscan) output and generates GFF3 features representing domains. Interesting tier for GBrowse.
-iprscan2gff3 $IPRout $NEWGFF >> $DIR/temp.gff
+iprscan2gff3 $IPRout $GFF3 >> $GFF3.funtemp.gff3
+
+mv $GFF3.funtemp.gff3 $GFF3.fun.gff3
+
+## 2. add blastp
 
 ## maker_functional_gff - Maps putative functions identified from BLASTP against UniProt/SwissProt to the MAKER produced GFF3 files in the Note attribute.
-maker_functional_gff $UNIPROTDB $BLASTPout $DIR/temp.gff > Gy_allnoM_rd3.maker_apocrita.noseq_corrected.gff.streamlined_for_AGAT.CURATED.gff
+maker_functional_gff $UNIPROTDB $BLASTPout $GFF3.fun.gff3 > $GFF3.funtemp.gff3
 
-## Remove double "Note"
-sed -i 's/;Note=Protein of unknown function;Note/;Note/g' Gy_allnoM_rd3.maker_apocrita.noseq_corrected.gff.streamlined_for_AGAT.CURATED.gff
+mv $GFF3.funtemp.gff3 $GFF3.fun.gff3
+
+## if there are 2 occurences of "Note=" in a sentence, remove the first occurence of ";Note=Protein of unknown function" if present
+sed '/Note=.*Note=/ {s/;Note=Protein of unknown function//}' $GFF3.fun.gff3 > $GFF3.funtemp.gff3
+
+mv $GFF3.funtemp.gff3 $GFF3.fun.gff3
 
 ## Convert gff to bed12 for further analyses
 conda activate transdecoder
 
-perl /data/home/btx915/.conda/envs/transdecoder/opt/transdecoder/util/gff3_file_to_bed.pl $DIR/Gy_allnoM_rd3.maker_apocrita.noseq_corrected.gff.streamlined_for_AGAT.CURATED.gff > $DIR/Gy_allnoM_rd3.maker_apocrita.noseq_corrected.gff.streamlined_for_AGAT.CURATED.transdec.bed12
+perl /data/home/btx915/.conda/envs/transdecoder/opt/transdecoder/util/gff3_file_to_bed.pl $GFF3.fun.gff3 > $GFF3.fun.bed12
 
 conda deactivate
 
-## And where is the blastp??
